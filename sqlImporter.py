5be8969a447bca66b03aa7f29ceec1f0fe0f34b4
AlessandroMessori <alessandro1.messori@mail.polimi.it>
2022-10-25 11:39:30 +0200

    completed SQL writer script

--- sqlImporter.py
+++ sqlImporter.py
@@ -1,4 +1,5 @@
 import json
+import hashlib
 from sqlDBInit import create_connection
 
 tube_data_file = open('./train-network.json')
@@ -28,33 +29,61 @@ def writeStationsTable(stations, conn):
         print("There was a mistake during the writing of the records in the DB")
         print(e)
 
-def writeLinesTable(lines, cursor, conn):
+def writeLinesTable(lines, conn):
+
+    cursor = conn.cursor()
 
     linesRows = []
+    lineToStationsRow = []
 
     for line in lines:
-        stationRows.append((
-            station["id"], 
-            station["name"]
+        currentLineID = getIdFromName(line["name"])
+
+        linesRows.append((
+            currentLineID,
+            line["name"]
         ))
 
-    insertQuery = """ insert ignore into Stations (
-         ) 
-        values (%s,%s)           
-    """
+        for stationID in line["stations"]:
+            lineToStationsRow.append((
+                currentLineID,
+                stationID
+             )
+            )
+
+    insertQueryLines = """ insert into Lines(id, name) values (?, ?) """
 
     try:
-        cursor.executemany(insertQuery, stationRows)
+        cursor.executemany(insertQueryLines, linesRows)
         conn.commit()
-    except:
+        print("Lines data inserted into SQLite DB correctly")
+    except Exception as e:
         conn.rollback()
+        print("There was a mistake during the writing of the Line records in the DB")
+        print(e)
+    
+    insertQueryLinesToStations = """ insert into LinesToStations(lineID, stationID) values (?, ?) """
 
+    try:
+        cursor.executemany(insertQueryLinesToStations, lineToStationsRow)
+        conn.commit()
+        print("Lines data inserted into SQLite DB correctly")
+    except Exception as e:
+        conn.rollback()
+        print("There was a mistake during the writing of the LineToStations records in the DB")
+        print(e)
+
+def getIdFromName(name):
+    m = hashlib.md5()
+    m.update(name.encode('utf-8'))
+    return m.hexdigest()
 
 if __name__ == '__main__':
     conn = create_connection("train_network")
     cursor = conn.cursor()
 
-    writeStationsTable(tube_data["stations"], conn)
+    #writeStationsTable(tube_data["stations"], conn)
+    writeLinesTable(tube_data["lines"], conn)
 
     conn.close()
 
