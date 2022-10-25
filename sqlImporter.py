9d7f9fe6c88bd3e97567420e94c3b24c5eda8604
AlessandroMessori <alessandro1.messori@mail.polimi.it>
2022-10-25 11:06:36 +0200

    wrote stations write to SQL function

--- sqlImporter.py
+++ sqlImporter.py
@@ -1,7 +1,28 @@
 import json
+from sqlDBInit import create_connection
 
 tube_data_file = open('./train-network.json')
 
 tube_data = json.load(tube_data_file)
 
-print(tube_data)
+def writeStationsTable(stations, cursor, conn):
+
+    stationRows = []
+
+    for station in stations:
+        stationRows.append((
+            station["id"], 
+            station["name"]
+        ))
+
+    insertQuery = """ insert ignore into Stations (
+         ) 
+        values (%s,%s)           
+    """
+
+    try:
+        cursor.executemany(insertQuery, stationRows)
+        conn.commit()
+    except:
+        conn.rollback()
+
